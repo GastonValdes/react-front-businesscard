561eb00e78e719526a704d2f3b30239285ae6b33
Gaston Valdes <gastonvaldes@gmail.com>
2020-03-01 00:01:30 -0300

    actualice columnas

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -5,44 +5,68 @@ import logo from '../logo.svg';
 
 const Contacts = ({contacts}) => {
     return (
-        <div>
-            <nav className="navbar navbar-dark bg-dark">
-          <a className="navbar-brand" href="/">
-          <img src={logo} className="Gv-logo" alt="logo" />
-            Tarjetas Personales
-            </a>
-        </nav>
-        {contacts.map((contact, i) => (
-          <div className="col-md-6" >
-        <div className="card mt-4">
-            <div className="card-title text-center">
-      <h5>{contact.nombre} {contact.apellido}</h5>
-               </div>
-           
-                <div className="card">
-                  <div className="card-header text-center">
-               {contact.empresa}
-               </div>     
-                    <div className="card-body text-left">
-               Puesto: {contact.puesto} <br/>
-               Celular: {contact.celular} <br/>
-               Telefono: {contact.telfijo} <br/>
-               email: {contact.email} <br/>
-               
-               
-            </div>
-            
-
-
-                </div>
-                </div>
-                </div>
-                
-            ))}
-        </div>
-        
+      <div>
+          <nav className="navbar navbar-dark bg-dark">
+              <a className="navbar-brand" href="/">
+                <img src={logo} className="Gv-logo" alt="logo" />
+                Tarjetas Personales
+              </a>
+          </nav>
+          
+          {contacts.map((contact, i) => (
+                                          <div class="container">
+                                           <div class="row">  
+                                          <div className="col-md-4 " >
+                                            <div className="card mt-4">
+                                              <div className="card">
+                                                <div className="card-body text-left">
+                                                  {contact.nombre} {contact.apellido}<br/>
+                                                  Empresa: {contact.empresa}<br/>
+                                                  Puesto: {contact.puesto} <br/>
+                                                  Celular: {contact.celular} <br/>
+                                                  Telefono: {contact.telfijo} <br/>
+                                                  email: {contact.email} <br/>
+                                                </div>
+                                             </div>
+                                            </div>
+                                          </div>
+                                          
+                                          <div className="col-md-4" >
+                                            <div className="card mt-4">
+                                              <div className="card">
+                                                <div className="card-body text-left">
+                                                  {contact.nombre} {contact.apellido}<br/>
+                                                  Empresa: {contact.empresa}<br/>
+                                                  Puesto: {contact.puesto} <br/>
+                                                  Celular: {contact.celular} <br/>
+                                                  Telefono: {contact.telfijo} <br/>
+                                                  email: {contact.email} <br/>
+                                                </div>
+                                             </div>
+                                            </div>
+                                          </div>
+                                          
+                                          <div className="col-md-4" >
+                                            <div className="card mt-4">
+                                              <div className="card">
+                                                <div className="card-body text-left">
+                                                  {contact.nombre} {contact.apellido}<br/>
+                                                  Empresa: {contact.empresa}<br/>
+                                                  Puesto: {contact.puesto} <br/>
+                                                  Celular: {contact.celular} <br/>
+                                                  Telefono: {contact.telfijo} <br/>
+                                                  email: {contact.email} <br/>
+                                                </div>
+                                             </div>
+                                            </div>
+                                          </div>
+                                          
+                                          </div>
+                                          </div>
+                                          ))}
+      
+      </div>
     )
-    
 };
 
 export default Contacts
